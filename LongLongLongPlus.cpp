f5f982b099861fa093a166861572bf00c98b8b35
DING JUNYU <43256899+DingJunyu@users.noreply.github.com>
2018-11-02 13:03:14 +0900

    コメント追加

--- LongLongLongPlus.cpp
+++ LongLongLongPlus.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 string LLLPlus(string A, string B) {
+	/*文字列の長さを統一し、足りない部分や頭の一位に０を入る*/
 	if (A.size() > B.size()) {
 		A.insert(0, 1, '0');
 		B.insert(0, A.size() - B.size(), '0');
@@ -13,16 +14,20 @@ string LLLPlus(string A, string B) {
 		A.insert(0, B.size() - A.size(), '0');
 	}
 
+	/*Bのイテレータを宣言*/
 	auto MarkB = B.end() - 1;
+	/*Aのイテレータを宣言し、ループ条件として設定する*/
 	for (auto it = A.end() - 1 ; it != A.begin(); --it) {
-		*it += *MarkB - '0';
-		--MarkB;
+		*it += *MarkB - '0';//重複な部分を削除
+		--MarkB;//Bのイテレータを一位前に移動
+		/*10を越えた時の処理*/
 		if (*it > '9') {
 			*it -= 10;
 			++*(it - 1);
 		}
 	}
+	/*最初の位に数字がいなければ、削除する*/
 	if (*A.begin() == '0')
 		A.erase(0, 1);
-	return A;
+	return A;//文字列Aを返す
 }
--- Merge.cpp
+++ Merge.cpp
@@ -6,28 +6,36 @@ void MergeExecution(int *Unsort,int Length) {
 	if (Length < 2)
 		return;
 
+	/*中点を取る*/
 	int Mid;
 	Mid = Length / 2;
-	int *L = new int[Mid];
+
+	/*前半と後半を別ける、数字をコピーする*/
+	int *L = new int[Mid];//動的にオブジェクトを生成
 	int *R = new int[Length - Mid];
 
 	for (int i = 0; i < Mid; ++i) L[i] = Unsort[i];
 	for (int i = Mid; i < Length; ++i) R[i - Mid] = Unsort[i];
 
+	/*別けた部分をソート*/
 	MergeExecution(L, Mid);
 	MergeExecution(R, Length - Mid);
+
 	int LeftArray = 0, RightArray = 0, Count = 0;
+	/*ソートされた配列を整理する*/
 	while (LeftArray < Mid&&RightArray < Length - Mid) {
+		/*小さいのは左置き*/
 		if (L[LeftArray] < R[RightArray]) 
 			Unsort[Count++] = L[LeftArray++];
 		else
 			Unsort[Count++] = R[RightArray++];
 	}
+	/*いずれが残ったら、そのまま配列の最後に追加する*/
 	while (LeftArray < Mid)
 		Unsort[Count++] = L[LeftArray++];
 	while (RightArray < Length - Mid)
 		Unsort[Count++] = R[RightArray++];
 
-	delete[] R;
+	delete[] R;//メモリ解放
 	delete[] L;
 }
